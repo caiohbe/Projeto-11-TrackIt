f4e1a918b0ff093738fd49735f5a4e60c8212f62
caiohbe <caiohbe@gmail.com>
2022-10-19 13:21:05 -0300

    RegisterPage css

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import LoginPage from "./components/LoginPage/LoginPage";
 import GlobalStyle from "./assets/styles/globalStyles";
+import RegisterPage from "./components/RegisterPage/RegisterPage";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Routes>
-        <Route path='/' element={<LoginPage />}></Route>
+        <Route path='/' element={<LoginPage />} />
+        <Route path="/cadastro" element={<RegisterPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/RegisterPage/RegisterPage.js
@@ -0,0 +1,67 @@
+import logo from '../../assets/images/logo.png'
+import styled from 'styled-components'
+import { secondaryColor } from '../../constants/colors'
+import { Link } from 'react-router-dom'
+
+function RegisterPage() {
+    return (
+        <Page>
+            <>
+                <img src={logo}></img>
+                <input type='text' placeholder='email'/>
+                <input type='text' placeholder='senha'/>
+                <input type='text' placeholder='nome'/>
+                <input type='text' placeholder='URL foto'/>
+
+                <button>Cadastrar</button>
+                <Link>
+                    <h3>Já tem uma conta? Faça login!</h3>
+                </Link>
+            </>
+        </Page>
+    )
+}
+
+export default RegisterPage
+
+const Page = styled.div`
+    font-family: 'Lexend Deca', sans-serif;
+
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    img {
+        margin-top: 70px
+    }
+
+    input {
+        margin-bottom: 8px;
+        border: 1px solid #D4D4D4;
+        padding-left: 11px;
+        box-sizing: border-box;
+
+        &::placeholder {
+            color: #DBDBDB;
+        }
+    }
+
+    input, button {
+        height: 45px;
+        font-size: 20px;
+        border-radius: 5px;
+        width: 80%;
+    }
+
+    button {
+        border: none;
+        background-color: ${secondaryColor};
+        color: #FFFFFF;
+        margin-bottom: 25px;
+    }
+
+    h3 {
+        font-size: 14px;
+        color: ${secondaryColor};
+    }
+`
